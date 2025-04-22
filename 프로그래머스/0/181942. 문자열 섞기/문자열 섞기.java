97ca2c20e4e60bd261d7bc2a466fb92d7293541f
kig7174 <kig7174@naver.com>
2025-04-22 11:42:31 +0900

    [level 0] Title: 문자열 섞기, Time: 37.48 ms, Memory: 95.1 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -1,11 +1,10 @@
 class Solution {
     public String solution(String str1, String str2) {
         String answer = "";
-        String[] s1 = str1.split("");
-        String[] s2 = str2.split("");
-        
-        for (int i=0; i<s1.length; i++) {
-            answer += s1[i] + s2[i];
+
+        for (int i=0; i<str1.length(); i++) {
+            answer += str1.charAt(i);
+            answer += str2.charAt(i);
         }
         return answer;
     }
