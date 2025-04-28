30365b699bb37f42641adeec0cc6d26bb74ae85d
kig7174 <kig7174@naver.com>
2025-04-28 10:21:28 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.03 ms, Memory: 90.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int a = 0;
+        int b = 0;
+        for (int i=0; i<num_list.length; i++) {
+            if ((i + 1) % 2 == 0) a += num_list[i];
+            else b += num_list[i];
+        }
+        return a >= b ? a : b;
+    }
+}
