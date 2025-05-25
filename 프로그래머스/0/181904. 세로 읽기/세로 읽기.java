7716b7da5b0238341c80c6016b64209981b948fb
kig7174 <kig7174@naver.com>
2025-05-25 22:38:01 +0900

    [level 0] Title: 세로 읽기, Time: 17.46 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
@@ -1,8 +1,7 @@
 class Solution {
     public String solution(String my_string, int m, int c) {
         String answer = "";
-        int a = my_string.length();
-        for(int i = c - 1; i < a; i += m) {
+        for(int i = c - 1; i < my_string.length(); i += m) {
             answer += my_string.charAt(i);
         }
         return answer;
